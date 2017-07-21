942f8c071ede2afb93b91737ac195b673f44d7de
Nicholas Mu <nhmu@andrew.cmu.edu>
2017-07-21 14:01:58 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "UKFkPukAHqlCwfgFeOq8T7q6J"
+consumer_secret = "lUWkSKG96HlgKf0cAc57dul9i6oFyoGOOUef22XraSFx4q4n6c"
+access_key = "4332745696-uFWQWu8KHxjWP3sOMaOXHWpWfKOs9Zvg7pIRpVV"
+access_secret = "Hz3j63WLhvHSgYMlAwfkQiqMUZUeHmg9YotlpBhCc2Iht"
